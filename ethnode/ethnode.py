0b5aa236468591b4c1cbf6e7af1435816f6abbbb
hardcode <dobri.stoilov@gmail.com>
2017-10-18 20:20:28 +0300

    add http socket_host to argv

--- ethnode/ethnode.py
+++ ethnode/ethnode.py
@@ -3,9 +3,14 @@ from websrv import httpsrv
 from multiprocessing import Process
 from toolkit import mockproc
 from toolkit import crypto
+import sys
 
 if __name__ == '__main__':
-    p1 = Process(target=httpsrv.main, args=('webdoc',))
+    socket_host = '127.0.0.1'
+    if len(sys.argv) == 2:
+        socket_host = sys.argv[1]
+
+    p1 = Process(target=httpsrv.main, args=('webdoc', socket_host))
     p2 = Process(target=mockproc.main, args=('p2', 1))
     p3 = Process(target=crypto.test_rsa_generic)
     p3.start()
--- ethnode/websrv/httpsrv.py
+++ ethnode/websrv/httpsrv.py
@@ -9,7 +9,7 @@ class EthearnalSite(object):
     # todo make entry point redirect to ui
 
 
-def main(webdoc: str = 'webdoc'):
+def main(webdoc: str = 'webdoc', socket_host='127.0.0.1'):
     conf = {
         '/': {
             'tools.sessions.on': True,
@@ -21,5 +21,10 @@ def main(webdoc: str = 'webdoc'):
             'tools.staticdir.index': 'index.html',
         }
     }
+    cherrypy.server.socket_host = socket_host
     cherrypy.quickstart(EthearnalSite(), '/', conf)
+
+
+if __name__ == '__main__':
+    main()
 
