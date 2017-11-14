947d4944772b2cf029350991f62355508baf47b6
hardcode <dobri.stoilov@gmail.com>
2017-11-14 09:55:21 +0200

    wip signed resource model

---
+++ ethnode/datamodel/base_sqlite.py
@@ -0,0 +1,43 @@
+import os
+import sqlite3
+import threading
+
+
+class BaseSQLite(object):
+    th_conn = dict()
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    @classmethod
+    def get_dbid(cls, db_name):
+        th_id = '%s:%s:%s:%s' % (db_name, os.getpid(), str(cls), str(threading.current_thread()))
+        return th_id
+
+    @classmethod
+    def get_connection(cls, db_name):
+        th_id = cls.get_dbid(db_name)
+        if th_id in cls.th_conn:
+            conn = cls.th_conn[th_id]
+            if conn:
+                return conn
+            else:
+                cls.th_conn[th_id] = sqlite3.connect(db_name)
+                return cls.th_conn[th_id]
+        else:
+            cls.th_conn[th_id] = sqlite3.connect(db_name)
+            return cls.th_conn[th_id]
+
+    @property
+    def dbid(self):
+        return self.get_dbid(self.db_name)
+
+    @property
+    def connection(self):
+        conn = self.get_connection(self.db_name)
+        return conn
+
+    @property
+    def cursor(self):
+        conn = self.get_connection(self.db_name)
+        return conn.cursor()
---
+++ ethnode/datamodel/resource_sqlite.py
@@ -0,0 +1,39 @@
+from datamodel.base_sqlite import BaseSQLite
+
+# encoded bin resource store
+# pk_hash = hash(owner,res,sig)
+# owner_hash_bin
+# resource_hash_bin
+# resource_owner_sig_bin
+# resource_codec_chain_bin
+# resource_data_bin
+# is_encrypted_bool
+
+
+class ResourceSQLite(BaseSQLite):
+    def __init__(self, db_name, table_name):
+        super(ResourceSQLite, self).__init__(db_name)
+        self.table_name = table_name
+
+    def create_table_if_not_exist(self, qs_only=False):
+        qs = '''
+        CREATE TABLE IF NOT EXISTS %s
+        (
+        pk_hash blob PRIMARY KEY,
+        owner_hash_bin blob,
+        resource_hash_bin blob,
+        resource_owner_sig_bin blob,
+        resource_codec_chain_bin blob,
+        resource_data_bin blob,
+        is_encrypted_bool bool
+        );
+        ''' % self.table_name
+
+        if qs_only:
+            return qs
+
+        else:
+            return self.cursor.execute(qs)
+
+
+
