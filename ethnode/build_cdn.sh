74bd0a484cdc0d17099be98b39bf3b067119b794
hardcode <dobri.stoilov@gmail.com>
2017-12-26 23:46:26 +0200

    cors fix

--- ethnode/build_cdn.sh
+++ ethnode/build_cdn.sh
@@ -4,16 +4,19 @@
 cwd=`pwd`
 target_package="cdn_linux_amd64.tar.gz"
 
+pyinstaller ./cdn.py
 pyinstaller ./ert.py
 sleep 3
 
-
+cp -r ./webdoc ./dist/ert/
+cp -r ./webui ./dist/ert/
 cp -r ./apidef ./dist/ert/
 cp -r ./cdnapidef ./dist/ert/
 
 mkdir -p ./packages
 cd ./dist
-tar zcvf ${target_package} ./ert
+cp -r ./ert/* ./cdn/
+tar zcvf ${target_package} ./cdn
 cp ${target_package} ../packages/
 cd ${cwd}
 
