512bc35f8a15a0819773c05ba2b427c22a0c8808
hardcode <dobri.stoilov@gmail.com>
2018-01-03 03:36:34 +0200

    fix

--- ethnode/dhtcdn/webapi.py
+++ ethnode/dhtcdn/webapi.py
@@ -138,18 +138,13 @@ class WebCDN(object):
     def on_post(self, hk_hex):
         self.dhf.push('', {'cdn_url': self.cdn_url,
                            'hk_hex': hk_hex,
-                           }, hk_hex=hk_hex, remote_only=True)
+                           }, hk_hex=hk_hex, remote_only=False)
 
     def try_get_meta(self, hkey):
 
         print('TRY GET META:', hkey)
         try:
             t = self.dhf.pull_remote(key='', hk_hex=hkey)
-            if not t:
-                print('TRY PULL AGAIN LOCAL')
-                from time import sleep
-                sleep(0.3)
-                t = self.dhf.pull_local(key='', hk_hex=hkey)
 
             if not t:
                 return
