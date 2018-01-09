cab1fb10c56b134aa74336d98ad5438f145efb8e
hardcode <dobri.stoilov@gmail.com>
2018-01-09 10:58:30 +0200

    profile info on found gigs

--- ethnode/webui/js/functions/modals/create/createGigToProfile.js
+++ ethnode/webui/js/functions/modals/create/createGigToProfile.js
@@ -89,11 +89,23 @@ function createGigToProfile2(hk, gig_o) {
 
 
 function createGigToFound(hk, gig_o) {
-    console.log("createGigToProfile2(hk,gig_o)",hk,gig_o);
+    console.log("createGigToFound(hk,gig_o)",hk, gig_o);
 
     var image ='';
     var gigID = hk;
     var api_cdn="http://london.ethearnal.com:5678/api/cdn/v1/resource?hkey=";
+    var default_profile_image = '1540b3c47ada5fd9a4798d3cc780d6a6bd05baf94cc2fdaa53e90accb4162383';
+    var profile_image = null;
+    var owner_guid = null;
+
+    console.log('gig_o',gig_o);
+    if(gig_o.hasOwnProperty('owner_guid')) {
+       owner_guid = gig_o.owner_guid;
+    }
+
+
+
+
 
     image += '<div class="ui fluid image"><div class="ui black ribbon label">';
     image += gig_o.general_domain_of_expertise +'</div><div class="image-block"><img src="'+api_cdn+'';
@@ -104,15 +116,40 @@ function createGigToFound(hk, gig_o) {
     var dropdownUL = '<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="DDB'+ hk +'"><li class="mdl-menu__item open-modal" open-modal="#edit-gig">Edit</li><li class="mdl-menu__item delete">Delete</li></ul>';
     var footer = '<div class="footer"><h4>Starting at <span><i class="material-icons">polymer</i>' + gig_o.price + '</span></h4></div>';
 
-    //console.log('GIG TO PROFILE', gig_o);
-    // $gig = $('<div id="'+gigID+'" class="gig content-block" data-toggle="modal" data-target="#gigModal">' + dropdownButton + dropdownUL + image +  title + desc + footer + '</div>');
-    //gig_ctx = 0 | $("#"+hk);
-    //gig_ctx = document.getElementById(hk);
-    gig_html = '<div id="'+gigID+'" class="gig content-block" data-toggle="modal" data-target="#gigModal">' + dropdownButton + dropdownUL + image +  title + desc + footer + '</div>';
-    //
-    // console.log('RENDER GIG', gigID);
+    var img_src = api_cdn + default_profile_image ;
+    console.log('IMG+SRC', img_src);
+    // var img_src = '';
 
-    $(".gigs-container").append(gig_html);
+    var img_txt = '<div class="owner-info">'
+                + '<img id="imgav' + gigID + '" src="' + img_src + '" alt="Avatar">'
+                + '<h5>owner_nickname</h5>'
+                + '<h4>Entry Level</h4>'
+                + '</div>' ;
+
+    //img_txt = '"<img id="avatar-img'+gigID+'" src="' + img_src + '" alt="Bobby brown" class="profile-picture">';
+
+    gig_html = '<div id="'+gigID+'" class="gig content-block" data-toggle="modal" data-target="#gigModal">'
+               + dropdownButton + dropdownUL + image +  title + img_txt + desc + footer + '</div>';
+
+   $(".gigs-container").append(gig_html);
     componentHandler.upgradeDom();
+
+    if(gig_o.hasOwnProperty('owner_guid')) {
+       //
+       owner_guid = gig_o.owner_guid;
+       console.log('++ + ++ ', owner_guid);
+       getProfileValue(owner_guid, 'profilePicture', function(profilePictureURL) {
+            //
+            console.log('profileID', owner_guid);
+
+            console.log('profilePC', profilePictureURL);
+            p_src = api_cdn + JSON.parse(profilePictureURL);
+             console.log('ELEMENT', p_src);
+            el = $("#imgav"+gigID);
+            el.attr('src', p_src);
+            console.log('ELEMENT', el);
+        });
+
+    }
 
 }
