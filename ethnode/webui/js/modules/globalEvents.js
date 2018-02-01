3083bc1659fb11aea95767bdd0c8fd9d12e174c7
Andriy Khrobak <andrew.khrobak@gmail.com>
2018-02-01 20:12:01 +0200

    quick update

--- ethnode/webui/js/modules/globalEvents.js
+++ ethnode/webui/js/modules/globalEvents.js
@@ -1,8 +1,14 @@
 // Global Events
-
+$(document).click(function(e) {
+    if (!$(e.target).closest('.dropdown').length) {
+        $('#settings-dropdown').parents('.dropdown').removeClass('show');
+    }
+});
 // Dropdown show in header
 $(document).on('click', '#settings-dropdown', function(e) {
     e.preventDefault();
+    e.stopPropagation();
     $(this).parents('.dropdown').toggleClass('show');
+
     // .siblings('[data-labelledby=settings-dropdown]').
 });
