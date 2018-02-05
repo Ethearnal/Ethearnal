4afdb08f5645f21c443539e8576a303fcc76de29
Andriy Khrobak <andrew.khrobak@gmail.com>
2018-02-05 16:33:33 +0200

    quick update + 1 route /profiles

--- ethnode/webui/src/js/main.js
+++ ethnode/webui/src/js/main.js
@@ -2,6 +2,7 @@ require("./modules/smartSearch.js"); // MODULE FOR SEARCHING
 require("./modules/globalEvents.js"); // MODULE with GLOBAL EVENTS
 require("./modules/imageCrop.js"); // MODULE with IMAGE CROPPER
 require("./modules/onInitProfile.js"); // MODULE with INIT PROFILE PAGE
+require("./modules/onInitProfiles.js"); // MODULE with INIT PROFILE PAGE
 require("./modules/generateGigs.js"); // MODULE with Gigs generator
 
 import obj from './showExport';
