82ee496ed722798a0b1d53e30592cf3ae02694d2
hardcode <dobri.stoilov@gmail.com>
2018-02-05 12:22:50 +0200

    clean

--- ethnode/adryui/js/functions/getData/profilePortfolios.js
+++
@@ -1,9 +0,0 @@
-// GETS PORTFOLIOS ID BASED ON PROFILE ID
-function getProfilePortfolios(profileID, callback) {
-    $.ajax({
-        type: 'GET',
-        url: '/api/v1/dht/portfolios/?owner_guid=' + profileID,
-        dataType: 'text',
-        success: callback
-    });
-}
--- ethnode/adryui/js/modules/globalEvents.js
+++
@@ -1,8 +0,0 @@
-// Global Events
-
-// Dropdown show in header
-$(document).on('click', '#settings-dropdown', function(e) {
-    e.preventDefault();
-    $(this).parents('.dropdown').toggleClass('show');
-    // .siblings('[data-labelledby=settings-dropdown]').
-});
