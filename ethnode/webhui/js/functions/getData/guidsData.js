ec0f603237255ed901f81520a933689925691efd
Andriy Khrobak <andrew.khrobak@gmail.com>
2018-02-06 13:26:10 +0200

    removing /webhui

--- ethnode/webhui/js/functions/getData/guidsData.js
+++
@@ -1,9 +0,0 @@
-// GETS ALL GUIDS (NODES) DATA
-function getGuidsData(callback) {
-    $.ajax({
-        type: 'GET',
-        url: '/api/v1/dht/guids',
-        dataType: 'text',
-        success: callback
-    });
-}
