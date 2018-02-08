c3a01f10b0a7339130d815b8d2195b4f0cd5080a
hardcode <dobri.stoilov@gmail.com>
2018-02-08 02:09:32 +0200

    fix deploy profile script

--- ethnode/sh/dpl.sh
+++ ethnode/sh/dpl.sh
@@ -2,9 +2,13 @@
 
 #!/bin/bash
 FILES=/eth/gihub/Ethearnal/ethnode/data_demo/json_data/*.json
+PROFILE_DATA=/eth/gihub/Ethearnal/ethnode/data_demo/profile_data/
 for f in $FILES
 do
   echo "Processing $f file..."
+  name=$(basename "$f")
+  # name=${name} | tr . _
+  echo ${name}
   # take action on each file. $f store current file name
-  python ert.py -n -i -l 127.0.0.1:2001 -u 127.0.0.1:3001 -j $f  -d data_demo/$1_p -s 127.0.0.1:45678
+  python ert.py -n -i -l 127.0.0.1:2001 -u 127.0.0.1:3001 -j $f  -d ${PROFILE_DATA}/${name}_p -s 127.0.0.1:45678 &>/dev/null
 done
