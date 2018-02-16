8b89e75c614cbdf050c52addcf4e87eb084f76ab
AndrewSparrowww <voolve@gmail.com>
2018-02-16 15:52:40 +0200

    finished newGigModal

--- ethnode/webui/src/js/modules/newGigModal.js
+++ ethnode/webui/src/js/modules/newGigModal.js
@@ -1,6 +1,7 @@
 window.createNewGig = (function() {
   $("#add-gig").find("#newGigexpire").datepicker();
     $("#add-gig").on("hidden.bs.modal", function(e) {
+      $(this).find(".gig-tags").find(".delete").click();
       $(this)
         .find("img#input-image-gig").hide().addClass("empty").end()
         .find(".img-label").show().removeClass("active").text('').end()
@@ -11,7 +12,7 @@ window.createNewGig = (function() {
         .find(".range-slider")
         .find("input").val("0").end()
         .find(".range-slider__value").text("0").end()
-        .find("#new-gig-category").parent().find(".text").text("All Categories").end();
+        .find("#new-gig-category").parent().find(".text").text("All Categories");
     });
     
 
