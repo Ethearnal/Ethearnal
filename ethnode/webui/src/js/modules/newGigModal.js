9675eac7f8872f242bc316d968d2addbd9bab2d1
AndrewSparrowww <voolve@gmail.com>
2018-02-16 15:34:15 +0200

    added reseting form for newGigModal

--- ethnode/webui/src/js/modules/newGigModal.js
+++ ethnode/webui/src/js/modules/newGigModal.js
@@ -1,3 +1,18 @@
 window.createNewGig = (function() {
   $("#add-gig").find("#newGigexpire").datepicker();
+    $("#add-gig").on("hidden.bs.modal", function(e) {
+      $(this)
+        .find("img#input-image-gig").hide().addClass("empty").end()
+        .find(".img-label").show().removeClass("active").text('')end()
+        .find("#cropper-wrap-gig").hide().end()
+        .find($(".btns-wrap").find(".btn-success")).hide();
+      $(this)
+        .find("input,textarea,select").val("").end()
+        .find(".range-slider")
+        .find("input").val("0").end()
+        .find(".range-slider__value").text("0").end()
+        .find("#new-gig-category").parent().find(".text").text("All Categories").end();
+    });
+    
+
 })();
