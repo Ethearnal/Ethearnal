014dd2f7c6f157f814162ebe690bd496b357f767
hardcode <dobri.stoilov@gmail.com>
2018-03-09 21:06:29 +0200

    update bash scr

--- ethnode/sh/cdn/scr-std-i.sh
+++ ethnode/sh/cdn/scr-std-i.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-screen -S CDN ./sh/cdn/std-i.sh $1 $2
+screen -S CDN "./sh/cdn/std-i.sh $1 $2"
--- ethnode/sh/cdn/std-i.sh
+++ ethnode/sh/cdn/std-i.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# $1 cdn_profiliei nstance
-# $2 cdn file dir instance
+echo " < $1 > cdn_profile < $2 > files dir"
 
-python cdn.py -f -d ~/cdn_files_$2 -p ~/cdn_profile_$1 eth0 -l 0.0.0.0:5678 -c http://159.65.56.140:8080/friendface.json -i
+python cdn.py -f -d ~/cdn_files_$2 -p ~/cdn_profile_$1 -f eth0 -l 0.0.0.0:5678 -c http://159.65.56.140:8080/friendface.json -i
