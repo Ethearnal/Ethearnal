2e2be88ccb0f6d8889680b93376b0ef01bc4e246
hardc0d3 <dobri.stoilov@gmail.com>
2018-03-10 14:39:11 +0200

    wip ert: nod script

--- ethnode/sh/nod/nod.sh
+++ ethnode/sh/nod/nod.sh
@@ -3,6 +3,7 @@
 pro=$1
 cdn_to_use=$2
 udp_boot_to=$3
-python ert.py -f eth0 -l 0.0.0.0:4567 -u 0.0.0.0:34567 -d ~/nod_profile_${pro} -p ${cdn_to_use} -i -s ${udp_boot_to}
+#python ert.py -l 0.0.0.0:4567 -u 0.0.0.0:34567 -d ~/nod_profile_p -i -f eth0 -s 188.166.154.183:45678 -p 188.166.154.183:5678
+python ert.py -i -f eth0 -l 0.0.0.0:4567 -u 0.0.0.0:34567 -d ~/nod_profile_${pro} -p ${cdn_to_use} -s ${udp_boot_to}
 
 
