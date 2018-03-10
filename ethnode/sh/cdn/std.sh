9d0ead9e84c06790e39d87c69828286aedcf185d
hardcode <dobri.stoilov@gmail.com>
2018-03-10 08:19:51 +0200

    wip: sh stuff testing

--- ethnode/sh/cdn/std.sh
+++ ethnode/sh/cdn/std.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-$1
-profile_dir=$1
-python ert.py -f eth0 -u 0.0.0.0:34567 -l 0.0.0.0:4567 -i -d ~/nod_profile_${profile_dir}
+
+pro=$1
+fls=$2
+python cdn.py -d ~/cdn_files_${fls} -p ~/cdn_profile_${pro} -f eth0 -l 0.0.0.0:5678 -i
