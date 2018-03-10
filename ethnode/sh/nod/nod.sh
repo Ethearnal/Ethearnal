7d041cb3a02e96328cce87b588822f3748262fa7
hardc0d3 <dobri.stoilov@gmail.com>
2018-03-10 13:30:42 +0200

    wip ert: node

--- ethnode/sh/nod/nod.sh
+++ ethnode/sh/nod/nod.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #!/usr/bin/env bash
 pro=$1
-python ert.py -f eth0 -l 0.0.0.0:4567 -u 0.0.0.0:34567 -d ~/nod_profile_${pro} -i
+cdn_to_use=$2
+python ert.py -f eth0 -l 0.0.0.0:4567 -u 0.0.0.0:34567 -d ~/nod_profile_${pro} -p ${cdn_to_use} -i
 
 
