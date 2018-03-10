d296354e9c8ce5fd6da2ce8530430c00676ba5de
hardcode <dobri.stoilov@gmail.com>
2018-03-10 08:57:04 +0200

    sh: test

--- ethnode/sh/nod/nod.sh
+++ ethnode/sh/nod/nod.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #!/usr/bin/env bash
 pro=$1
-python ert.py -f eth0 -l 0.0.0.0:4567 -u 0.0.0.0:34567 -d ~/nod_profile_${pro}
+python ert.py -f eth0 -l 0.0.0.0:4567 -u 0.0.0.0:34567 -d ~/nod_profile_${pro} -i
+
 
