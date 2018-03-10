7f0e08f4506123736cdaadfa07379da784aa4c19
hardcode <dobri.stoilov@gmail.com>
2018-03-10 05:06:25 +0200

    wip: refactor resource api sync

--- ethnode/webfacades/dht_kv.py
+++ ethnode/webfacades/dht_kv.py
@@ -587,6 +587,8 @@ class WebCDNRefactorWebCdnResourceApi(WebApiBase):
             cdn_host_port = v.get('cdn_host_port')
             cdn_mount_point = v.get('cdn_resource_endpoint')
             if cdn_host_port and cdn_mount_point:
+                self.rcli.host_port = cdn_host_port
+                self.rcli.endpoint = cdn_mount_point
                 hk_hex_from_dht = v.get('hk_hex')
                 if hk_hex != hk_hex_from_dht:
                     print('INTEGRITY ERR on hkeys')
